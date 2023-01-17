80c1b6160dfc67e978862e721309d5a56bea82b2
mibr2d <hodakov7419@gmail.com>
2023-01-17 16:43:09 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// Не использовать строки для расчета.
+
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = InputInt("Введите число");
+int a = number / 100;
+int b = (number % 100) / 10;
+int e = number % 10;
+
+Console.WriteLine($"{a}{e}");
