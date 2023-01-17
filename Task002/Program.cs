a600dea001cc7fe1de7ebeed6b68fd4100a10d28
mibr2d <hodakov7419@gmail.com>
2023-01-17 12:45:43 +0300

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры 
+// нет. Не использовать строки для расчета.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = InputInt("Введите целое  число");
+if (number > 99 && number < 1000)
+{
+    int result = number%10;
+    Console.WriteLine("Последняя цифра числа равна " + result);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
